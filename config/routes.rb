295e87ae91dc649b8db6ce15497f0219ca6fc6eb
Ten Mutunhire <tmutunhire@gmail.com>
2014-11-05 19:53:01 +0000

    add device routes for login and join

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'bookmarks#index'
-
+  
+  devise_scope :user do
+    get '/login' => 'devise/sessions#new'
+    get '/join' => 'devise/registrations#new'
+  end
+    
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
